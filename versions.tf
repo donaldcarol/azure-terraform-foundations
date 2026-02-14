cf5b6ac1a7d585f916adc279137c0df409d27bdd
donaldcarol <donald@dakini.network>
2026-02-14 19:03:07 +0200

    Configure remote backend (Azure Storage) and migrate state

--- versions.tf
+++ versions.tf
@@ -7,4 +7,14 @@ terraform {
       version = "~> 3.100"
     }
   }
+
+ backend "azurerm" {
+    resource_group_name  = "rg-tf-state"
+    storage_account_name = "sttfstate111"
+    container_name       = "tfstate"
+    key                  = "azure-terraform-foundations.tfstate"
+  }
+
+
+
 }
